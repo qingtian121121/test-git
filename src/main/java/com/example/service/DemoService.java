f846830655030146be7b2915f98ee0fb902803b2
冶彤伟 <yetongwei@newrank.cn>
2023-09-01 10:34:14 +0800

    第一次上传github

---
+++ src/main/java/com/example/service/DemoService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : YeTongWei
+ * @className : DemoService
+ * @data : 2023-09-01
+ * @description:
+ */
+@Component
+public class DemoService {
+    public static void main(String[] args) {
+        System.out.println("第一次上传github");
+    }
+}
