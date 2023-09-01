d21cfd854ae728f11de691dae3d86270ea423227
冶彤伟 <yetongwei@newrank.cn>
2023-09-01 11:39:22 +0800

    回退远程仓库的代码

--- src/main/java/com/example/service/DemoService.java
+++ src/main/java/com/example/service/DemoService.java
@@ -13,7 +13,5 @@ public class DemoService {
     public static void main(String[] args) {
         System.out.println("第一次上传github");
         System.out.println("第二次上传github");
-        //第三次上传并且推送到远程仓库
-        System.out.println("第三次上传github");
     }
 }
