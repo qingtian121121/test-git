f3e11a7ed539186b5b4159364d77da980a66e327
冶彤伟 <yetongwei@newrank.cn>
2023-09-01 10:42:13 +0800

    第二次上传github

--- src/main/java/com/example/service/DemoService.java
+++ src/main/java/com/example/service/DemoService.java
@@ -12,5 +12,6 @@ import org.springframework.stereotype.Component;
 public class DemoService {
     public static void main(String[] args) {
         System.out.println("第一次上传github");
+        System.out.println("第二次上传github");
     }
 }
